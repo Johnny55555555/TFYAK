7d65e7ac372cb591882598f00300f6cce30b4715
Johnny55555555 <151951482+Johnny55555555@users.noreply.github.com>
2026-03-11 14:30:57 +0700

    Create Token.cs

---
+++ ТФЯК №1/Properties/Token.cs
@@ -0,0 +1,19 @@
+namespace ТФЯК__1
+{
+    public class Token
+    {
+        public int Code { get; set; }
+
+        public string Type { get; set; }
+
+        public string Lexeme { get; set; }
+
+        public string Position { get; set; }
+
+        public int Line { get; set; }
+
+        public int Start { get; set; }
+
+        public int End { get; set; } 
+    }
+}
