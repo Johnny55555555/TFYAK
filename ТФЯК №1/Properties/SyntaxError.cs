d0d0cfbc1033e8772cf2953c65069c2d65f4cb12
Johnny55555555 <151951482+Johnny55555555@users.noreply.github.com>
2026-03-26 05:25:06 +0700

    Create SyntaxError.cs

---
+++ ТФЯК №1/Properties/SyntaxError.cs
@@ -0,0 +1,15 @@
+namespace ТФЯК__1
+{
+    public class SyntaxError
+    {
+        public string Fragment { get; set; }
+
+        public string Position { get; set; }
+
+        public string Description { get; set; }
+
+        public int Line { get; set; }
+
+        public int Column { get; set; }
+    }
+}
